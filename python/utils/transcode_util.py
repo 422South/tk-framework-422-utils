ae59b9ca22e270bb22a0f0e8972f2d83436f4cd2
craig <craig@422south.com>
2023-12-06 11:04:47 +0000

    Added vide_transcode_audio for After Effects audio

--- python/utils/transcode_util.py
+++ python/utils/transcode_util.py
@@ -39,6 +39,54 @@ def sequence_transcode(fileName, pathToImageSequence, outputFilePath, frame_rate
     return
 
 
+def video_transcode_audio(fileName, pathToImageSequence, outputFilePath, audio_file, audio_start, start_frame, duration, frame_rate=25):
+    fontSize = 38
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    ffmpegPath = os.path.join(current_dir, 'ffmpeg')
+    logoPath = os.path.join(current_dir, '422Logo.png')
+    fontPath = os.path.join(current_dir, 'Moderat-Regular.otf').replace('\\', '/').replace(
+        ':', '\:')
+    # fontPath requires some weird formatting to work inside subprocess for ffmpeg.exe
+    # The : needs to be escaped, no other backslashes can be present
+    if float(start_frame) - float(audio_start) < 0:
+        audioStartTime = '-0' + str(
+            datetime.timedelta(seconds=(abs(float(start_frame) - float(audio_start))) / float(frame_rate)))
+    else:
+        audioStartTime = '0' + str(
+            datetime.timedelta(seconds=(float(start_frame) - float(audio_start)) / float(frame_rate)))
+
+    audioDuration = '0' + str(datetime.timedelta(seconds=(duration/ float(frame_rate))))
+
+    call_args = [ffmpegPath, '-r', str(frame_rate), '-gamma', '2.2', '-i', pathToImageSequence, '-i', logoPath,
+                 '-ss', audioStartTime, '-i', audio_file,
+                 '-filter_complex',
+                 "[0:v][1:v]overlay=(main_w-overlay_w)-10:10,"
+                 "drawtext=fontsize=" + str(
+                     fontSize) + ":x=10:y=h-th-10:fontcolor=White:fontfile='" + fontPath + "':text='" + fileName.replace(
+                     '_Tags.mp4', '') + "',"
+                                        "drawtext=fontsize=" + str(
+                     fontSize) + ":x=w-tw-10:y=h-th-10:fontcolor=White:fontfile='" + fontPath + "':text='Frame\: %{n}':start_number=1",
+                 '-pix_fmt', 'yuv420p',
+                 '-b:v', '30000k', '-to', audioDuration,outputFilePath, '-y']
+
+    extension = pathToImageSequence.split('.')[-1]
+    if extension and extension.lower() in ['tif', 'tiff']:
+        call_args = [ffmpegPath, '-r', str(frame_rate), '-i', pathToImageSequence, '-i', logoPath,
+                     '-ss', audioStartTime, '-i', audio_file,
+                     '-filter_complex',
+                     "[0:v][1:v]overlay=(main_w-overlay_w)-10:10,"
+                     "drawtext=fontsize=" + str(
+                         fontSize) + ":x=10:y=h-th-10:fontcolor=White:fontfile='" + fontPath + "':text='" + fileName.replace(
+                         '_Tags.mp4', '') + "',"
+                                            "drawtext=fontsize=" + str(
+                         fontSize) + ":x=w-tw-10:y=h-th-10:fontcolor=White:fontfile='" + fontPath + "':text='Frame\: %{n}':start_number=1",
+                     '-pix_fmt', 'yuv420p',
+                     '-b:v', '30000k', '-to', audioDuration, outputFilePath, '-y']
+
+    subprocess.call(call_args)
+    return
+
+
 def video_transcode(fileName, pathToImageSequence, outputFilePath, frame_rate=25):
     fontSize = 38
     current_dir = os.path.dirname(os.path.realpath(__file__))
